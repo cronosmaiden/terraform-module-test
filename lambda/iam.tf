cf71f582c3cff44db15c68669e8efea72a1174c6
Christian David Morales Mayorga <cmoralem@ternium.com.mx>
2025-01-19 14:12:02 -0500

    update

--- lambda/iam.tf
+++ lambda/iam.tf
@@ -3,7 +3,7 @@
 ############################
 
 resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda-execution-role-new2"
+  name = "lambda-execution-role-new3"
 
   # Permitir que Lambda asuma este rol
   assume_role_policy = jsonencode({
