2932b8c89fba6966c5636aae3f6d95d67992514d
Christian David Morales Mayorga <cmoralem@ternium.com.mx>
2025-01-20 00:30:51 -0500

    add lambda function name

--- lambda/outputs.tf
+++ lambda/outputs.tf
@@ -12,3 +12,8 @@ output "lambda_execution_role_arn" {
   description = "ARN del rol de ejecución de Lambda creado."
   value       = aws_iam_role.lambda_execution_role.arn
 }
+
+output "lambda_function_name" {
+  description = "Nombre de la función Lambda creada."
+  value       = aws_lambda_function.lambda.function_name
+}
